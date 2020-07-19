fc996d7793a469901b5ba348959fb76949d90445
Wendy Diard <wendy@diard.us>
2020-07-19 12:21:40 -0400

    Added mysql to test hello file

--- hello.js
+++ hello.js
@@ -1,3 +1,35 @@
-console.log("Hello World!");
-console.log("How are you?");
 
+
+var mysql=require('mysql');
+
+// create a connection
+var con=mysql.createConnection({
+	host: "localhost",
+	user: "test",
+	password: "test",
+	database: "test",
+
+})
+
+
+// connect to mysql
+con.connect(function(err) {
+	if (err) throw err;
+	console.log("Connected to Database");
+
+	var sql="Insert into tbl_customers (cust_email, cust_name) values ?";
+	var values=[
+		['tim@tim.com','Tim'],
+		['sam@sam.com','Sam'],
+		['laura@laura.com','Laura'],
+
+	]
+	con.query(sql, [values], function(err, result) {
+		if (err) throw err;
+		console.log("records inserted: "+result.affectedRows);
+	})
+
+})
+
+
+console.log("Hello World!");
